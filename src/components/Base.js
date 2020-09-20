2a7db814207a8af7c38bb95566cfac6af0da7d48
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-20 20:31:25 +0700

    framer motion practice

---
+++ src/components/Base.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariant = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: {
+      ease: "easeInOut",
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
+
+const hoverVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const Base = ({ addBase, pizza }) => {
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+  return (
+    <motion.div
+      className="base container"
+      variants={containerVariant}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h3>Step 1: Choose Your Base</h3>
+      <ul>
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
+          return (
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
+        })}
+      </ul>
+
+      {pizza.base && (
+        <motion.div className="next" variants={nextVariants}>
+          <Link to="/toppings">
+            <motion.button variants={hoverVariants} whileHover="hover">
+              Next
+            </motion.button>
+          </Link>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+};
+
+export default Base;
